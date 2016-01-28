f8cc30550a35da381901b141462ffd9070bd4e69
Ricardo Bocchi <ricardo@mobilemind.com.br>
2016-01-28 11:36:46 -0200

    sharing

--- facebook.android.js
+++ facebook.android.js
@@ -3,6 +3,7 @@ var _isInit = false;
 var _AndroidApplication = applicationModule.android;
 var _act;
 var mCallbackManager;
+var mFailCallback
 var loginManager;
 function init(loginBehavior) {
     try {
@@ -28,6 +29,7 @@ function init(loginBehavior) {
 exports.init = init;
 function registerCallback(successCallback, cancelCallback, failCallback) {
     if (_isInit) {
+        mFailCallback = failCallback
         var act = _AndroidApplication.foregroundActivity || _AndroidApplication.startActivity;
         _act = act;
         loginManager.registerCallback(mCallbackManager, new com.facebook.FacebookCallback({
@@ -90,4 +92,35 @@ exports.share = function(params){
     var content = builder.build();     
 
     com.facebook.share.widget.ShareDialog.show(activity, content)
+}
+
+exports.requestUserProfile = function(accessToken, fields, done){   
+
+    console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
+
+    var request = com.facebook.GraphRequest.newMeRequest(accessToken, new com.facebook.GraphRequest.GraphJSONObjectCallback({        
+        onCompleted: function(user, graphResponse) {
+
+            if(graphResponse.getError()){
+                var errorString = "Message: " + graphResponse.getError().getErrorMessage() 
+                errorString += ", Type: " + graphResponse.getError().getErrorType()
+                errorString += ", Code: " + graphResponse.getError().getErrorCode()
+                mFailCallback(errorString)
+            }else{
+                var userJson = {
+                    email: user.optString("email"),
+                    name: user.optString("name"),
+                    id: user.optString("id"),
+                    token: accessToken.getToken()              
+                }
+
+                done(userJson)            
+            }
+        }
+    }))
+
+    var parameters = new android.os.Bundle();
+    parameters.putString("fields", fields);    
+    request.setParameters(parameters);
+    request.executeAsync()
 }
