bf54d71fc75bc7feb52936d5d8e7dab61fb31e9d
Ricardo Bocchi <ricardobocchi@gmail.com>
2016-03-29 17:31:14 -0300

    ios

--- facebook.android.js
+++ facebook.android.js
@@ -1,65 +1,279 @@
 var applicationModule = require("application");
 var _AndroidApplication = applicationModule.android;
 
-var facebookApi 
+var Facebook = function(){
+    
+    Facebook.logInWithPublishPermissions = function(permissions) {
+        if (this._isInit) {
+            var javaPermissions = java.util.Arrays.asList(permissions);
+            this.loginManager.logInWithPublishPermissions(this._act, javaPermissions);
+        }
+    }
 
-exports.init = function(loginBehavior) {
-    facebookApi = new Facebook()
-    facebookApi.initSdk(loginBehavior)
-}
+    Facebook.logInWithReadPermissions = function(permissions) {
+        if (this._isInit) {
+            var javaPermissions = java.util.Arrays.asList(permissions);
+            this.loginManager.logInWithReadPermissions(this._act, javaPermissions);
+        }
+    }    
 
-exports.registerCallback = function(successCallback, cancelCallback, failCallback) {
-    facebookApi.registerCallback(successCallback, cancelCallback, failCallback)
-}
+    Facebook.getAcessToken = function(){
+        return com.facebook.AccessToken.getCurrentAccessToken();
+    }
 
-exports.logInWithPublishPermissions = function(permissions) {
-    facebookApi.logInWithPublishPermissions(permissions)
-}
-
-exports.logInWithReadPermissions = function(permissions) {
-    facebookApi.logInWithReadPermissions(permissions)
-}
-
-exports.share = function(params){
-    facebookApi.share(params)    
-}
-
-exports.requestUserProfile = function(fields, doneCallback){   
-
-    //console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
-
-    var parameters = new android.os.Bundle();
-    parameters.putString("fields", fields);    
-
-    var accessToken = facebookApi.getAcessToken()
-
-    facebookApi.doMeRequest(accessToken, parameters, function(fbUser){
-        var json = toJson(fbUser)
-        json.token = accessToken.getToken()              
-        doneCallback(json)
-    })   
-}
-
-exports.requestBooks = function(fields, doneCallback){   
-
-    //console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
-
-    var graphPath = "/me/books.reads"
-    var accessToken = facebookApi.getAcessToken()
-
-    facebookApi.doGraphPathRequest(accessToken, graphPath, function(entities){
-        var json = toJsonrray(entities)        
-        doneCallback(json)
-    })   
-}
+    Facebook.isLoggedIn = function(){
+        return this.getAcessToken() != null
+    }
 
 
-exports.isLoggedIn = function(){
-    return facebookApi.isLoggedIn()
-}
+    Facebook.logout = function(){
+        if(this._isInit)
+            this.loginManager.logOut();
+    }
 
-exports.getToken = function(){
-    facebookApi.getAcessToken()
+    Facebook.initSdk = function(loginBehavior){
+
+        if(this._isInit)
+            return true
+
+        try {
+            com.facebook.FacebookSdk.sdkInitialize(_AndroidApplication.context.getApplicationContext());
+        }catch (error) {
+            console.log("nativescript-facebook-login: The plugin could not find the android library, try to clean the android platform. " + error);
+        }
+                
+        this.mCallbackManager = com.facebook.CallbackManager.Factory.create();
+        this.loginManager = com.facebook.login.LoginManager.getInstance();            
+
+        if (loginBehavior) 
+            this.loginManager = this.loginManager.setLoginBehavior(loginBehavior);
+        
+
+        if (this.mCallbackManager && this.loginManager) {
+            this._isInit = true;
+            return true;
+        }else {
+            return false;
+        }          
+    }
+
+    Facebook.registerCallback = function(successCallback, cancelCallback, failCallback){
+        this._failCallback  = failCallback
+        this._successCallback  = successCallback
+        this._cancelCallback = cancelCallback
+
+        if (this._isInit) {            
+            var self = this
+            this._act = _AndroidApplication.foregroundActivity || _AndroidApplication.startActivity;
+            
+            this.loginManager.registerCallback(this.mCallbackManager, new com.facebook.FacebookCallback({
+                onSuccess: function (result) {
+                    self._successCallback(result);
+                },
+                onCancel: function () {
+                    self._cancelCallback();
+                },
+                onError: function (e) {
+                    self._failCallback(e);
+                }
+            }));
+            
+            this._act.onActivityResult = function (requestCode, resultCode, data) {
+                self.mCallbackManager.onActivityResult(requestCode, resultCode, data);
+            };
+        }        
+    }   
+
+    // args = { fields, doneCallback}
+    Facebook.requestUserProfile = function(args){   
+
+        //console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
+
+        var parameters = new android.os.Bundle();
+        if(args.fields)
+            parameters.putString("fields", args.fields);    
+
+        var accessToken = this.getAcessToken()
+
+        this.doMeRequest(accessToken, parameters, function(fbUser){
+            var json = toJson(fbUser)
+            json.token = accessToken.getToken()              
+            args.doneCallback(json)
+        })   
+    }
+
+    Facebook.requestBooks = function(fields, doneCallback){   
+
+        //console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
+
+        var graphPath = "/me/books.reads"
+        var accessToken = this.getAcessToken()
+
+        this.doGraphPathRequest(accessToken, graphPath, function(entities){
+            var json = toJsonrray(entities)        
+            doneCallback(json)
+        })   
+    }     
+
+    //url, title, content, imageUrl
+    Facebook.share = function(params){
+        try{
+            var activity = _AndroidApplication.foregroundActivity
+            var builder = new com.facebook.share.model.ShareLinkContent.Builder()            
+            
+            if(params.url)
+                builder.setContentUrl(android.net.Uri.parse(params.url))
+
+            if(params.title)
+                builder.setContentTitle(params.title)
+
+            if(params.imageUrl)
+                builder.setImageUrl(android.net.Uri.parse(params.imageUrl))
+
+            if(params.content)
+                builder.setContentDescription(params.content)
+
+            var content = builder.build();     
+
+            com.facebook.share.widget.ShareDialog.show(activity, content)   
+        }catch(error){
+            console.log(error)
+            this._failCallback(error)
+        }     
+    }
+
+    // imagePath, content, imageUrl
+    Facebook.sharePhoto = function(params){
+        try{
+            var activity = _AndroidApplication.foregroundActivity
+        
+            var builder = new com.facebook.share.model.SharePhotoContent().Builder()
+            var photo = this.createPhotoShare(params)
+            var content = builder.addPhoto(photo).build();
+
+            com.facebook.share.widget.ShareDialog.show(activity, content)  
+        }catch(error){
+            console.log(error)
+            this._failCallback(error)
+        }      
+    }  
+
+    // list of {imagePath, content, imageUrl}
+    Facebook.sharePhotos = function(args){
+        try{
+            var activity = _AndroidApplication.foregroundActivity
+            
+            var builder = new com.facebook.share.model.SharePhotoContent().Builder()
+            var photos = []
+
+            for(var i in args.list){
+                photos.push(this.createPhotoShare(args.list[i]))
+            }
+
+            for(var i in photos)
+                builder.addPhoto(photos[i])
+
+            var content = builder.build();
+
+            com.facebook.share.widget.ShareDialog.show(activity, content) 
+        }catch(error){
+            console.log(error)
+            this._failCallback(error)
+        }       
+    }  
+
+    Facebook.createPhotoShare = function(params){
+        var builder = new com.facebook.share.model.SharePhoto.Builder();
+        var bmOptions = new android.graphics.BitmapFactory.Options();
+        var bitmap
+
+        if(params.imagePath)
+            bitmap = android.graphics.BitmapFactory.decodeFile(params.imagePath, bmOptions);        
+
+        if(bitmap)
+            builder.setBitmap(bitmap);
+
+        if(params.content)
+            builder.setCaption(params.content);
+
+        if(params.content)
+            builder.setCaption(params.content);
+
+        if(params.imageUrl)
+            builder.setImageUrl(android.net.Uri.parse(params.imageUrl))
+
+        builder.setUserGenerated(true);
+
+        return builder.build();     
+    }  
+
+    Facebook.doMeRequest = function(accessToken, budle, doneCallback){   
+
+        try{
+            console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
+
+            var self = this
+            var request = com.facebook.GraphRequest.newMeRequest(accessToken, new com.facebook.GraphRequest.GraphJSONObjectCallback({        
+                onCompleted: function(entity, graphResponse) {
+
+                    console.log("## Facebook onCompleted")
+
+                    if(graphResponse.getError()){                    
+                        self.handlerError(graphResponse.getError())
+                    }else{
+                        doneCallback(entity)            
+                    }
+                }
+            }))
+
+            if(budle)
+                request.setParameters(budle);
+
+            request.executeAsync()
+        }catch(error){
+            console.log(error)
+            this._failCallback(error)
+        }
+    }
+
+    Facebook.doGraphPathRequest = function(accessToken, graphPath, doneCallback){   
+
+        try{
+            console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
+            //var graphPath = "/me/books.reads"
+            
+            var self = this
+            var request = com.facebook.GraphRequest.newGraphPathRequest(accessToken, graphPath, new com.facebook.GraphRequest.GraphJSONArrayCallback({        
+                onCompleted: function(entities, graphResponse) {
+
+                    if(graphResponse.getError()){
+                        self.handlerError(graphResponse.getError())
+                    }else{                    
+                        doneCallback(entities)            
+                    }
+                }
+            }))
+
+            request.executeAsync()
+        }catch(error){
+            console.log(error)
+            this._failCallback(error)
+        }
+    }    
+
+
+    Facebook.handlerError = function(error){
+
+        var errorString = "Message: " + error.getErrorMessage() 
+        errorString += ", Type: " + error.getErrorType()
+        errorString += ", Code: " + error.getErrorCode()
+
+        if(this._failCallback)
+            this._failCallback(errorString)
+
+    }
+
+    return Facebook
 }
 
 function toJson(entity){
@@ -103,171 +317,6 @@ function toJsonrray(entities){
     }
 
     return jsonArray
-}
-
-
-var Facebook = function(){
-    
-    Facebook.logInWithPublishPermissions = function(permissions) {
-        if (this._isInit) {
-            var javaPermissions = java.util.Arrays.asList(permissions);
-            this.loginManager.logInWithPublishPermissions(this._act, javaPermissions);
-        }
-    }
-
-    Facebook.logInWithReadPermissions = function(permissions) {
-        if (this._isInit) {
-            var javaPermissions = java.util.Arrays.asList(permissions);
-            this.loginManager.logInWithReadPermissions(this._act, javaPermissions);
-        }
-    }    
-
-    Facebook.getAcessToken = function(){
-        return com.facebook.AccessToken.getCurrentAccessToken();
-    }
-
-    Facebook.isLoggedIn = function(){
-        return this.getAcessToken() != null
-    }
-
-
-    Facebook.logout = function(){
-        if(this._isInit)
-            this.loginManager.logOut();
-    }
-
-    Facebook.initSdk = function(loginBehavior){
-
-        if(this._isInit)
-            return true
-
-        try {
-            com.facebook.FacebookSdk.sdkInitialize(_AndroidApplication.context.getApplicationContext());
-        }catch (e) {
-            console.log("nativescript-facebook-login: The plugin could not find the android library, try to clean the android platform");
-        }
-                
-        this.mCallbackManager = com.facebook.CallbackManager.Factory.create();
-        this.loginManager = com.facebook.login.LoginManager.getInstance();            
-
-        if (loginBehavior) 
-            this.loginManager = this.loginManager.setLoginBehavior(loginBehavior);
-        
-
-        if (this.mCallbackManager && this.loginManager) {
-            this._isInit = true;
-            return true;
-        }else {
-            return false;
-        }          
-    }
-
-    Facebook.registerCallback = function(successCallback, cancelCallback, failCallback){
-        this._failCallback  = failCallback
-        this._successCallback  = successCallback
-        this._cancelCallback = cancelCallback
-
-        if (this._isInit) {            
-            var self = this
-            this._act = _AndroidApplication.foregroundActivity || _AndroidApplication.startActivity;
-            
-            this.loginManager.registerCallback(this.mCallbackManager, new com.facebook.FacebookCallback({
-                onSuccess: function (result) {
-                    self._successCallback(result);
-                },
-                onCancel: function () {
-                    self._cancelCallback();
-                },
-                onError: function (e) {
-                    self._failCallback(e);
-                }
-            }));
-            
-            this._act.onActivityResult = function (requestCode, resultCode, data) {
-                self.mCallbackManager.onActivityResult(requestCode, resultCode, data);
-            };
-        }        
-    }    
-
-    //contentURL, contentTitle, imageURL, contentDescription
-    Facebook.share = function(params){
-        var activity = _AndroidApplication.foregroundActivity
-        var builder = new com.facebook.share.model.ShareLinkContent.Builder()            
-        
-        if(params.contentURL)
-            builder.setContentUrl(android.net.Uri.parse(params.contentURL))
-
-        if(params.contentTitle)
-            builder.setContentTitle(params.contentTitle)
-
-        if(params.imageURL)
-            builder.setImageUrl(android.net.Uri.parse(params.imageURL))
-
-        if(params.contentDescription)
-            builder.setContentDescription(params.contentDescription)
-
-        var content = builder.build();     
-
-        com.facebook.share.widget.ShareDialog.show(activity, content)        
-    }
-
-    Facebook.doMeRequest = function(accessToken, budle, doneCallback){   
-
-        console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
-
-        var self = this
-        var request = com.facebook.GraphRequest.newMeRequest(accessToken, new com.facebook.GraphRequest.GraphJSONObjectCallback({        
-            onCompleted: function(entity, graphResponse) {
-
-                console.log("## Facebook onCompleted")
-
-                if(graphResponse.getError()){                    
-                    self.handlerError(graphResponse.getError())
-                }else{
-                    doneCallback(entity)            
-                }
-            }
-        }))
-
-        if(budle)
-            request.setParameters(budle);
-
-        request.executeAsync()
-    }
-
-    Facebook.doGraphPathRequest = function(accessToken, graphPath, doneCallback){   
-
-        console.log("### accessToken.getPermissions()=" + accessToken.getPermissions())
-        //var graphPath = "/me/books.reads"
-        
-        var self = this
-        var request = com.facebook.GraphRequest.newGraphPathRequest(accessToken, graphPath, new com.facebook.GraphRequest.GraphJSONArrayCallback({        
-            onCompleted: function(entities, graphResponse) {
-
-                if(graphResponse.getError()){
-                    self.handlerError(graphResponse.getError())
-                }else{                    
-                    doneCallback(entities)            
-                }
-            }
-        }))
-
-        request.executeAsync()
-    }    
-
-
-    Facebook.handlerError = function(error){
-
-        var errorString = "Message: " + error.getErrorMessage() 
-        errorString += ", Type: " + error.getErrorType()
-        errorString += ", Code: " + error.getErrorCode()
-
-        if(this._failCallback)
-            this._failCallback(errorString)
-
-    }
-
-    return Facebook
 }
 
 exports.Facebook = Facebook
--- facebook.ios.js
+++ facebook.ios.js
@@ -1,23 +1,4 @@
-
-var facebookApi;
-
-exports.init = function(loginBehavior) {
-    if(!facebookApi){
-        facebookApi = new Facebook()
-        facebookApi.initSdk(loginBehavior)
-    }
-}
-exports.registerCallback = function(successCallback, cancelCallback, failCallback) {
-    facebookApi.registerCallback(successCallback, cancelCallback, failCallback)
-}
-
-exports.logInWithPublishPermissions = function(permissions) {
-    facebookApi.logInWithPublishPermissions(permissions)
-}
-
-exports.logInWithReadPermissions = function(permissions) {
-    facebookApi.logInWithReadPermissions(permissions)
-}
+var application = require('application')
 
 var Facebook = function(){
 
@@ -34,10 +15,16 @@ var Facebook = function(){
     }
 
     Facebook.getAccessToken = function(){
-        return FBSDKAccessToken.currentAccessToken()
+        console.log("getAccessToken")
+        var accessToken = FBSDKAccessToken.currentAccessToken()
+
+        console.log("getAccessToken accessToken=" + accessToken)
+
+        return accessToken
     }
 
     Facebook.isLoggedIn = function(){
+        console.log("isLoggedIn")
         return this.getAccessToken() != null
     }
 
@@ -70,16 +57,16 @@ var Facebook = function(){
         if (this._isInit) {
             this._callbackManager = function (result, error) {
                 if (error) {
-                    console.log("login error: " + error)
-                    // https://developers.facebook.com/docs/ios/errors
-                    var message = error.userInfo[FBSDKErrorDeveloperMessageKey]
-                    self._failCallback(message);
+                    self.handlerError(error)
                     return;
                 }
                 if (!result) {
                     self._failCallback("Null error");
                     return;
                 }
+
+                console.log("## result=" + result)
+
                 if (result.isCancelled) {
                     self._cancelCallback();
                     return;
@@ -95,22 +82,207 @@ var Facebook = function(){
         }
     }
 
+    // args = {fields , doneCallback }
+    Facebook.requestUserProfile = function(args){   
+
+        this.doMeRequest({
+            params: {
+                fields: args.fields || {}
+            },
+            doneCallback: function(result){            
+                console.log("## result=" + result)
+                args.doneCallback({
+                    email: result.objectForKey('email'),
+                    id: result.objectForKey('id'),
+                    name: result.objectForKey('name'),
+                })
+            }
+        })   
+    }    
+
+    // url, title, content, imageUrl
     Facebook.share = function(params){
 
+        try{
+            var content = FBSDKShareLinkContent.alloc().init()
+
+           if(params.url)
+                content.contentURL = NSURL.URLWithString(params.url)
+
+            if(params.title)
+                content.contentTitle = params.title
+
+            if(params.content)
+                content.contentDescription = params.content
+
+            if(params.imageUrl){                
+                content.imageURL = NSURL.URLWithString(params.imageUrl)
+            }
+
+            var view = application.ios.rootController
+
+            var mydelegate = this.createSharingDelegate()
+
+            FBSDKShareDialog.showFromViewControllerWithContentDelegate(view, content, mydelegate);
+        }catch(error){
+            console.log("## error=" + error)
+            this._failCallback(error)
+        }
+    }
+
+    // imagePath, content, imageUrl
+    Facebook.sharePhoto = function(args){
+        try{
+
+            if(!this.isInstalled()){
+                this._failCallback("facebook app not installed")
+                return
+            }
+
+            var content = FBSDKSharePhotoContent.alloc().init()
+
+            var photo = this.createPhotoShare(args)
+            content.photos = [photo];    
+            var view = application.ios.rootController
+            
+            
+            var mydelegate = this.createSharingDelegate()
+
+            FBSDKShareDialog.showFromViewControllerWithContentDelegate(view, content, mydelegate);
+
+        }catch(error){
+            console.log("## error=" + error)
+            this._failCallback(error)            
+        }
+    }
+
+    // args = list of {imagePath, content, imageUrl} 
+    Facebook.sharePhotos = function(args){
+
+        try{
+
+            if(!this.isInstalled()){
+                this._failCallback("facebook app not installed")
+                return
+            }
+            
+          var content = FBSDKSharePhotoContent.alloc().init()
+          var photos = []
+
+          for(var i in args.list){
+            photos.push(this.createPhotoShare(args.list[i]))
+          }
+
+          content.photos = photos;    
+
+
+          var view = application.ios.rootController
+
+          var mydelegate = this.createSharingDelegate()
+          FBSDKShareDialog.showFromViewControllerWithContentDelegate(view, content, mydelegate);
+
+        }catch(error){
+            console.log("## error=" + error)
+            this._failCallback(error)            
+        }
+    }
+
+    Facebook.isInstalled = function(){
+        var nsUrl = NSURL.URLWithString("fbapi://")
+        var isInstalled = UIApplication.sharedApplication().canOpenURL(nsUrl)
+        return isInstalled
+    }    
+
+    Facebook.createPhotoShare = function(args){
+
+        var photoShare = FBSDKSharePhoto.alloc().init()
+
+        if(args.imagePath)
+            photoShare.image = UIImage.imageWithContentsOfFile(args.imagePath);
+
+        if(args.imageUrl)
+            photoShare.imageURL = NSURL.URLWithString(args.imageUrl)
+
+        if(args.content)
+            photoShare.caption = args.content
+        
+        photoShare.userGenerated = true;
+
+        return photoShare
     }
 
     Facebook.doMeRequest = function(args){
-
+        args.path = "me"        
+        this.doGraphPathRequest(args)
     }
 
     Facebook.doGraphPathRequest = function(args){
+        var self = this
+        args.params = args.params || {}
 
+        console.log("## doGraphPathRequest path=" + args.path + ", args.params=" + JSON.stringify(args.params))
+        try{            
+            if(this.isLoggedIn()){
+                var request = FBSDKGraphRequest.alloc().initWithGraphPathParameters(args.path, args.params);
+                request.startWithCompletionHandler(function(connection, result, error){
+
+                    if(error){
+                        self.handlerError(error)
+                        return
+                    }
+
+                    if(!result){
+                        self._failCallback("Null error")
+                        return
+                    }
+
+                    args.doneCallback(result)
+
+                })
+            }else{
+                self._failCallback("app is not logged in")
+            }
+        }catch(error){
+            console.log("## error=" + error)
+            this._failCallback(error)
+        }
     }
 
     Facebook.handlerError = function(error){
-
+        console.log("login error: " + error)                    
+        // https://developers.facebook.com/docs/ios/errors
+        var message = error.userInfo.objectForKey(FBSDKErrorDeveloperMessageKey)
+        this._failCallback(message);        
     }
 
+    Facebook.createSharingDelegate = function(){
+        var self = this
+        var MySharingDelegate = (function (_super) {
+            __extends(MySharingDelegate, _super);
+            function MySharingDelegate() {
+                _super.apply(this, arguments);
+            }
+            MySharingDelegate.prototype.sharerDidCompleteWithResults = function (sharer, results) {
+                console.log("## delegate sharerDidCompleteWithResults")
+            };
+            MySharingDelegate.prototype.sharerDidFailWithError = function (sharer, error) {
+                console.log("## delegate sharerDidFailWithError " + error)
+                self._failCallback(error)
+            };
+            MySharingDelegate.prototype.sharerDidCancel = function (sharer) {
+                console.log("## delegate sharerDidCancel")
+            };
+
+            MySharingDelegate.ObjCProtocols = [FBSDKSharingDelegate];
+
+            return MySharingDelegate;
+
+        }(UIResponder)); 
+
+        return new MySharingDelegate()   
+    }
     return Facebook
 
 }
+
+exports.Facebook = Facebook
